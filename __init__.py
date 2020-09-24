c9743218b2d1690b7fcecb54e91bc057c00d4f5e
bndeepankan <bn.deepankan@gmail.com>
2020-09-25 01:04:02 +0200

    changed create_app settings

--- __init__.py
+++ __init__.py
@@ -5,39 +5,42 @@ import os
 from flask import Flask, redirect, url_for
 from db import get_db
 
-def create_app(test_config=None):
+# def create_app(test_config=None):
     # Create and configure the app
 
-    app = Flask(__name__, instance_relative_config=True)
+app = Flask(__name__, instance_relative_config=True)
 
-    app.config.from_mapping(
-        SECRET_KEY='dev',
-        DATABASE=os.path.join(app.instance_path, 'Expense_Tracker.mongo')
-    )  
+app.config.from_mapping(
+    SECRET_KEY='dev',
+    DATABASE=os.path.join(app.instance_path, 'Expense_Tracker.mongo')
+)  
 
-    @app.route('/')
-    def index():
-        return "Hello World!!!"
+@app.route('/')
+def index():
+    return "Hello World!!!"
 
-    if test_config is None:
-        # load the instance config, if it exists, when not testing
-        app.config.from_pyfile('config.py', silent=True)
-    else:
-        # load the test config if passed in
-        app.config.from_mapping(test_config)
+# if test_config is None:
+#     # load the instance config, if it exists, when not testing
+#     app.config.from_pyfile('config.py', silent=True)
+# else:
+#     # load the test config if passed in
+#     app.config.from_mapping(test_config)
 
-    # ensure the instance folder exists
-    try:
-        os.makedirs(app.instance_path)
-    except OSError:
-        pass
+# ensure the instance folder exists
+try:
+    os.makedirs(app.instance_path)
+except OSError:
+    pass
 
-    from . import auth
-    app.register_blueprint(auth.bp)
+from . import auth
+app.register_blueprint(auth.bp)
 
-    from .import expense
-    app.register_blueprint(expense.bp)
-    
-    app.add_url_rule('/select_expense', endpoint='select_operation')
+from .import expense
+app.register_blueprint(expense.bp)
 
-    return app
+app.add_url_rule('/select_expense', endpoint='select_operation')
+
+    # return app
+
+if __name__ == '__main__':
+    app.run(debug=True)
