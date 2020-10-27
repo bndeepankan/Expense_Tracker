f515a98d619fd79fbf52816b3d6243bd9e207379
bndeepankan <bn.deepankan@gmail.com>
2020-10-28 00:53:28 +0100

    changed settings

--- __init__.py
+++ __init__.py
@@ -3,7 +3,7 @@ This app implements a money tracking tool and exposes a RESTful API meant to be 
 """
 import os
 from flask import Flask, redirect, url_for
-from .db import get_db
+from db import get_db
 
 app = Flask(__name__, instance_relative_config=True)
 
